9c11f9983ec14dfbdd5cf9a71887513455606f48
Johannes <johannes.bluhm@outlook.com>
2022-04-11 16:52:03 +0200

    Print a value

---
+++ Johannes/Playground.hs
@@ -0,0 +1,40 @@
+
+data Company = C [Dept]
+data Dept = D Name Manager [SubUnit]
+data SubUnit = PU Employee | DU Dept
+data Employee = E Person Salary
+data Person = P Name Address
+data Salary = S Float
+type Manager = Employee
+type Name = String
+type Address = String
+
+genCom :: Company
+genCom = C [D "Research" ralf [PU joost, PU marlow], D "Strategy" blair []]
+ralf, joost, marlow, blair :: Employee
+ralf = E (P "Ralf" "Amsterdam") (S 8000)
+joost = E (P "Joost" "Amsterdam") (S 1000)
+marlow = E (P "Marlow" "Cambridge") (S 2000)
+blair = E (P "Blair" "London") (S 100000)
+
+x :: Person 
+x = (\(E person _) -> person) blair
+
+y :: Name
+y = (\(P name _) -> name) x
+
+
+
+main :: IO ()
+main = putStrLn $ "The name is: " ++ y
+
+-- increase k (C ds) = C (map (incD k) ds)
+-- incD :: Float -> Dept -> Dept
+-- incD k (D nm mgr us) = D nm (incE k mgr) (map (incU k) us)
+-- incU :: Float -> SubUnit -> SubUnit
+-- incU k (PU e) = PU (incE k e)
+-- incU k (DU d) = DU (incD k d)
+-- incE :: Float -> Employee -> Employee
+-- incE k (E p s) = E p (incS k s)
+-- incS :: Float -> Salary -> Salary
+-- incS k (S s) = S (s * (1+k))

dff969b52f8196a384ef45297c785648e5c37d0d
Johannes <johannes.bluhm@outlook.com>
2022-05-27 17:49:49 +0200

    increase fnc

--- Johannes/Playground.hs
+++ Johannes/Playground.hs
@@ -1,4 +1,5 @@
 
+module Main where
 data Company = C [Dept]
 data Dept = D Name Manager [SubUnit]
 data SubUnit = PU Employee | DU Dept
@@ -17,6 +18,23 @@ joost = E (P "Joost" "Amsterdam") (S 1000)
 marlow = E (P "Marlow" "Cambridge") (S 2000)
 blair = E (P "Blair" "London") (S 100000)
 
+increase :: Float -> Company -> Company
+
+increase k (C ds) = C (map (incD k) ds)
+incD :: Float -> Dept -> Dept
+incD k (D nm mgr us) = D nm (incE k mgr) (map (incU k) us)
+incU :: Float -> SubUnit -> SubUnit
+incU k (PU e) = PU (incE k e)
+incU k (DU d) = DU (incD k d)
+incE :: Float -> Employee -> Employee
+incE k (E p s) = E p (incS k s)
+incS :: Float -> Salary -> Salary
+incS k (S s) = S (s * (1+k))
+
+main = print $ increase 0.1 genCom
+
+--main :: IO ()    -- This says that main is an IO action.
+--main = return () -- This tells main to do nothing.
 
 -- Code for outputting calculated stuff
 
@@ -31,22 +49,8 @@ blair = E (P "Blair" "London") (S 100000)
 --
 --personName :: Name
 --personName = (\(P name _) -> name) person
---
---
+
 --main :: IO ()
 --main = do
 --  print personName
 --  print outputSalary
-
-increase :: Float -> Company -> Company
-
-increase k (C ds) = C (map (incD k) ds)
-incD :: Float -> Dept -> Dept
-incD k (D nm mgr us) = D nm (incE k mgr) (map (incU k) us)
-incU :: Float -> SubUnit -> SubUnit
-incU k (PU e) = PU (incE k e)
-incU k (DU d) = DU (incD k d)
-incE :: Float -> Employee -> Employee
-incE k (E p s) = E p (incS k s)
-incS :: Float -> Salary -> Salary
-incS k (S s) = S (s * (1+k))
